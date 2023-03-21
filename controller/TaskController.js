7a2b5b92d2cb1ea38c923e25cd90efb07921accd
alanmolter <alanrenier@hotmail.com>
2023-03-20 22:06:39 -0300

    ""

---
+++ controller/TaskController.js
@@ -0,0 +1,83 @@
+const Task = require('../models/Task')
+
+const getAllTasks = async (req, res) => {//modulo criado 
+    try {
+        const taskList = await Task.find();
+        return res.render("index", { taskList, task: null, taskDelete: null });
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+const createTask = async (req, res) => {
+    const task = req.body;
+
+    if (!task.task) {
+        return res.redirect("/");
+    }
+
+
+
+    try {
+        await Task.create(task)
+        return res.redirect("/")
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+const getById = async (req, res) => {
+    //enviar o id como parametro da rota
+    try {
+        const taskList = await Task.find();
+
+        if(req.params.method == "update"){ //verifico a url, se for update entra no if
+        const task = await Task.findOne({ _id: req.params.id });
+        res.render("index", { task, taskDelete: null, taskList });
+        }else{
+            const taskDelete = await Task.findOne({_id: req.params.id});
+            res.render("index", {task: null, taskDelete, taskList});
+        }
+
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+const updateOneTask = async (req, res) => {//os parametros da requisição , precisão estar na ordem correta
+    try {
+        const task = req.body;
+        await Task.updateOne({ _id: req.params.id }, task);
+        res.redirect("/");//redireciona para página da lista de tarefas
+        return console.log("Tarefa atualizada com sucesso!")
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+
+
+
+const deleteOneTask = async (req,res) => {
+
+    try {
+
+        await Task.deleteOne({_id: req.params.id});
+        res.redirect("/");
+        return console.log("Tarefa excluida com sucesso");
+        
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+
+};
+
+
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getById,
+    updateOneTask,
+    deleteOneTask,//necessário a virgula na ultima função
+};
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require("path");
+const routes = require('./routes/routes.js');
+const conectToDb = require('./database/db.js');
+
+const doteenv = require('dotenv');
+doteenv.config();//funcao q indentifica o dot env
+
+conectToDb();
+const app = express();
+const port = 3000;
+
+
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(routes)
+
+
+/*app.get('/home', function (req, res) {
+    res.render('index');
+});*/
+
+//app.listen(3000);
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+})
